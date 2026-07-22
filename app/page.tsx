6ba2749dc11a2953b64dd3d22b8d49d1e2880d38
mojo_jojo <mojo@jojo.com>
2026-07-22 04:54:08 +0000

    refactored pages to match

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,11 @@
-import { AppSidebar } from "@/components/app-sidebar"
 import { CameraGrid } from "@/components/camera-grid"
 import { StatCards } from "@/components/stat-cards"
 
 export default function DashboardPage() {
   return (
     <>
-      <AppSidebar />
-      <main className="flex-1 overflow-y-auto p-6">
-        <StatCards />
-        <CameraGrid />
-      </main>
+      <StatCards />
+      <CameraGrid />
     </>
   )
 }
