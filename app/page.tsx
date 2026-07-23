302aba71fa6b4dd534fff9405b51069299010f7d
mojo_jojo <mojo@jojo.com>
2026-07-23 11:37:10 +0000

    allowed the main page to control its own scrolling

--- app/page.tsx
+++ app/page.tsx
@@ -3,9 +3,9 @@ import { StatCards } from "@/components/stat-cards"
 
 export default function DashboardPage() {
   return (
-    <>
+    <div className="h-full overflow-y-auto p-6">
       <StatCards />
       <CameraGrid />
-    </>
+    </div>
   )
 }
